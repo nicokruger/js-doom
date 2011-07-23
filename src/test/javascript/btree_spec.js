d0e5fe4b6b91c03e94e665e1ee3196223103238c
Nico Kruger <nico.kruger@rorotika.com>
2011-07-23 17:39:29 +0200

    Added a more complex line partitioning example.
    Seems the order is not correct, wonder what the ramifications are.

--- src/test/javascript/btree_spec.js
+++ src/test/javascript/btree_spec.js
@@ -49,22 +49,57 @@ describe("Basic polygon/BSP library", function() {
 
 	it("should be able to create a BSP from a more complex (non-convex) polygon", function() {
 
-        var v0 = $V(0,0)
-        var v1 = $V(2,0)
-        var v2 = $V(2,2)
-        var v3 = $V(1,2)
-        var v4 = $V(1,5)
-        var v5 = $V(5,5)
-        var v6 = $V(5,4)
-        var v7 = $V(7,4)
-        var v8 = $V(7,6)
-        var v9 = $V(0,6)
+        bsptree = this.Pp.bsp;
 
-        var Pp = $P(v0, v1, v2, v3, v4, v5, v6, v7, v8, v9)
+        // partition on the first edge
+        expect(bsptree.line).toEqual($L(this.v9, this.v0));
 
-        var bspnode;
+            expect(bsptree.positive).toEqual(null);
+            expect(bsptree.negative.line).toEqual($L(this.v0, this.v1))
+                expect(bsptree.negative.positive).toBe(null)
+                expect(bsptree.negative.negative.line).toEqual($L(this.v1,this.v2))
+                    expect(bsptree.negative.negative.positive.line).toEqual($L($V(2,5), this.v5))
+                        expect(bsptree.negative.negative.positive.positive.line).toEqual($L(this.v5, this.v6))
+                            expect(bsptree.negative.negative.positive.positive.positive).toBe(null)
+                            expect(bsptree.negative.negative.positive.positive.negative.line).toEqual($L(this.v6,this.v7))
+                                expect(bsptree.negative.negative.positive.positive.negative.positive).toBe(null)
+                                expect(bsptree.negative.negative.positive.positive.negative.negative.line).toEqual($L(this.v7, $V(7,5)))
+                                    expect(bsptree.negative.negative.positive.positive.negative.negative.negative).toBe(null)
+                                    expect(bsptree.negative.negative.positive.positive.negative.negative.positive).toBe(null)
+                        expect(bsptree.negative.negative.positive.negative.line).toEqual($L($V(7,5), this.v8))
+                            expect(bsptree.negative.negative.positive.negative.positive).toBe(null)
+                            expect(bsptree.negative.negative.positive.negative.negative.line).toEqual($L(this.v8, $V(2,6)))
+                                expect(bsptree.negative.negative.positive.negative.negative.positive).toBe(null)
+                                expect(bsptree.negative.negative.positive.negative.negative.negative).toBe(null)
+                    expect(bsptree.negative.negative.negative.line).toEqual($L(this.v2, this.v3))
+                        expect(bsptree.negative.negative.negative.positive.line).toEqual($L(this.v3, this.v4))
+                            expect(bsptree.negative.negative.negative.positive.positive.line).toEqual($L(this.v4, $V(2,5)))
+                                expect(bsptree.negative.negative.negative.positive.positive.positive).toBe(null)
+                                expect(bsptree.negative.negative.negative.positive.positive.negative.line).toEqual($L($V(2,6), $V(1,6)))
+                                    expect(bsptree.negative.negative.negative.positive.positive.negative.positive).toBe(null)
+                                    expect(bsptree.negative.negative.negative.positive.positive.negative.negative).toBe(null)
+                            expect(bsptree.negative.negative.negative.positive.negative.line).toEqual($L($V(1,6), this.v9))
+                                expect(bsptree.negative.negative.negative.positive.negative.positive).toBe(null);
+                                expect(bsptree.negative.negative.negative.positive.negative.negative).toBe(null);
+                        expect(bsptree.negative.negative.negative.negative).toBe(null)
 
-        /**************************************************************************************************************
+
+	})
+
+
+    it("should be able to correctly parition a line over a more complex BSP tree from a more complex (non-convex) polygon", function() {
+
+        var L = $L($V(-3,-3), $V(6,6))
+        var partition = this.Pp.partition(L);
+
+        expect(partition.pos).toEqual([$L($V(-3,-3), $V(0,0)), $L($V(2,2), $V(5,5))])
+        expect(partition.neg).toEqual([$L($V(5,5), $V(6,6)), $L($V(0,0), $V(2,2))])
+        expect(partition.codiff).toEqual([])
+        expect(partition.cosame).toEqual([])
+
+    })
+    beforeEach(function() {
+      /**************************************************************************************************************
          *    v9-----------v8
          *     |           |
          *     |           |
@@ -84,42 +119,21 @@ describe("Basic polygon/BSP library", function() {
          *
          **************************************************************************************************************/
 
-        bsptree = Pp.bsp;
+        this.v0 = $V(0,0)
+        this.v1 = $V(2,0)
+        this.v2 = $V(2,2)
+        this.v3 = $V(1,2)
+        this.v4 = $V(1,5)
+        this.v5 = $V(5,5)
+        this.v6 = $V(5,4)
+        this.v7 = $V(7,4)
+        this.v8 = $V(7,6)
+        this.v9 = $V(0,6)
 
-        // partition on the first edge
-        expect(bsptree.line).toEqual($L(v9, v0));
-
-            expect(bsptree.positive).toEqual(null);
-            expect(bsptree.negative.line).toEqual($L(v0, v1))
-                expect(bsptree.negative.positive).toBe(null)
-                expect(bsptree.negative.negative.line).toEqual($L(v1,v2))
-                    expect(bsptree.negative.negative.positive.line).toEqual($L($V(2,5), v5))
-                        expect(bsptree.negative.negative.positive.positive.line).toEqual($L(v5, v6))
-                            expect(bsptree.negative.negative.positive.positive.positive).toBe(null)
-                            expect(bsptree.negative.negative.positive.positive.negative.line).toEqual($L(v6,v7))
-                                expect(bsptree.negative.negative.positive.positive.negative.positive).toBe(null)
-                                expect(bsptree.negative.negative.positive.positive.negative.negative.line).toEqual($L(v7, $V(7,5)))
-                                    expect(bsptree.negative.negative.positive.positive.negative.negative.negative).toBe(null)
-                                    expect(bsptree.negative.negative.positive.positive.negative.negative.positive).toBe(null)
-                        expect(bsptree.negative.negative.positive.negative.line).toEqual($L($V(7,5), v8))
-                            expect(bsptree.negative.negative.positive.negative.positive).toBe(null)
-                            expect(bsptree.negative.negative.positive.negative.negative.line).toEqual($L(v8, $V(2,6)))
-                                expect(bsptree.negative.negative.positive.negative.negative.positive).toBe(null)
-                                expect(bsptree.negative.negative.positive.negative.negative.negative).toBe(null)
-                    expect(bsptree.negative.negative.negative.line).toEqual($L(v2, v3))
-                        expect(bsptree.negative.negative.negative.positive.line).toEqual($L(v3, v4))
-                            expect(bsptree.negative.negative.negative.positive.positive.line).toEqual($L(v4, $V(2,5)))
-                                expect(bsptree.negative.negative.negative.positive.positive.positive).toBe(null)
-                                expect(bsptree.negative.negative.negative.positive.positive.negative.line).toEqual($L($V(2,6), $V(1,6)))
-                                    expect(bsptree.negative.negative.negative.positive.positive.negative.positive).toBe(null)
-                                    expect(bsptree.negative.negative.negative.positive.positive.negative.negative).toBe(null)
-                            expect(bsptree.negative.negative.negative.positive.negative.line).toEqual($L($V(1,6), v9))
-                                expect(bsptree.negative.negative.negative.positive.negative.positive).toBe(null);
-                                expect(bsptree.negative.negative.negative.positive.negative.negative).toBe(null);
-                        expect(bsptree.negative.negative.negative.negative).toBe(null)
+        this.Pp = $P(this.v0, this.v1, this.v2, this.v3, this.v4, this.v5, this.v6, this.v7, this.v8, this.v9)
 
 
-	})
 
 
+    })
 })
