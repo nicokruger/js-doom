d1c0d658165be40ee31c42baf4cae68fe6589077
Nico Kruger <nico.kruger@rorotika.com>
2011-08-08 22:15:48 +0200

    Starting with JSON loading code... refactor in progress.

--- src/test/javascript/game_spec.js
+++ src/test/javascript/game_spec.js
@@ -1,18 +1,20 @@
 describe('3. The actual game',function(){
 
     describe("It should be possible to load a level, which is a list of polygons, from a JSON file", function() {
-        var d;
+        var polygons;
 
         runs(function() {
             $.getJSON("../../src/main/javascript/level/level1.json", function(data) {
-               d = data;
+               polygons = data["polygons"];
+            }).error(function(e) {
+                alert("error:" + e.statusText);
             });
-        })
+        });
 
-        waitsFor(function() { return d != null; }, "Could not retrieve example JSON level");
+        waitsFor(function() { return polygons != null; }, "Could not retrieve example JSON level");
 
         runs(function() {
-            expect(d["test"]).toBe("1");
+            expect(polygons[0]["points"].length).toBe(4);
         })
     });
 
