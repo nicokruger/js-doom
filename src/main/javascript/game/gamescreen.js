e00072b69359c95b074d973283546ff91a9d9698
Nico Kruger <nico.kruger@rorotika.com>
2011-08-20 08:24:38 +0200

    Started using quadtree in main renderer.
    The polygon rendering time is still a bottleneck. I'm not too sure
    whether it can be improved :(

--- src/main/javascript/game/gamescreen.js
+++ src/main/javascript/game/gamescreen.js
@@ -37,6 +37,29 @@ function createBlock(x,y, width, height) {
     return $P($V(x-width,y-width), $V(x+width,y-height), $V(x+width,y+height), $V(x-width,y+height))
 }
 
+function ZonePlacer(zone) {
+    var polyPlacer = PolyPlacer(zone.poly);
+
+    var operationInterceptor = function(operation) {
+        return function (polygon) {
+            operation(zone);
+        };
+    }
+    return {
+        topLeft: function (x,y,operation) {
+            polyPlacer.topLeft(x,y,operationInterceptor(operation));
+        },
+        topRight: function (x,y,operation) {
+            polyPlacer.topRight(x,y,operationInterceptor(operation));
+        },
+        bottomLeft: function (x,y,operation) {
+            polyPlacer.bottomLeft(x,y,operationInterceptor(operation));
+        },
+        bottomRight: function (x,y,operation) {
+            polyPlacer.bottomRight(x,y,operationInterceptor(operation));
+        }
+    }
+}
 function GameScreen(src) {
         hack = 0;
   var that = this;
@@ -55,16 +78,19 @@ function GameScreen(src) {
   this.towerObjects.forEach(function (tower) { that.gameComponents.push(tower) } );
 
 
-  var q1 = new Quad(2500,2500); var q2 = new Quad(75,75);
-  var q3 = new Quad(25,25); var q4 = new Quad(75,25);
-  var qt = new QuadTree(50, 50, q1, q2, q3, q4);
+  var bl = new Quad(250,250); var br = new Quad(750,250);
+  var tr = new Quad(750,750); var tl = new Quad(250,750);
 
-  this.quadtree = new QuadTree
+  this.quadtree = new QuadTree(500, 500, bl, br, tr, tl);
+
   var that = this;
   $.getJSON("data/doom.json", function(data) {
       alert("loaded level");
       that.zoneObjects = get_zones(data);
-      that.zoneObjects.forEach(function (zone) { that.gameComponents.push(zone) } );
+      that.zoneObjects.forEach(function (zone) {
+        that.gameComponents.push(zone);
+        that.quadtree.add(ZonePlacer(zone));
+      } );
 
   }).error(function(e) {
       alert("error:" + e.statusText);
@@ -105,13 +131,21 @@ function GameScreen(src) {
     }
 
     var t1 = Date.now();
-    for (var i in this.gameComponents) {
+    /*for (var i in this.gameComponents) {
       var tt1 = Date.now();
       this.gameComponents[i].draw(ctx);
       var tt2 = Date.now();
       console.log("Polygon time: " + (tt2-tt1));
-    }
-    console.log("Total time:" + (Date.now() - t1));
+    }*/
+    var i = 0;
+    this.quadtree.forEach(Square(0,500,500,1000), function (zone) {
+        var tt1 = Date.now();
+        zone.draw(ctx);
+        var tt2 = Date.now();
+        i++
+        console.log("   [" + zone.label + "] " + (tt2-tt1));
+    });
+    console.log("[ " + i + " polygons ] Total time:" + (Date.now() - t1));
   }
 
 
