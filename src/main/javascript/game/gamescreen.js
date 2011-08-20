9a4d0a029ec2b480856af85d8a2aa546863c157c
Nico Kruger <nico.kruger@rorotika.com>
2011-08-20 10:16:03 +0200

    Removed all non-doom related code.
    Im going ahead with optimising the rasterizer/renderer/btrees/quadtrees
    with the aim of creating a doom top down game. The aim is on getting the
    rendering times down.
    
    If we ever decide to go ahead with the GSM game, we will benefit from
    the development on this branch as well.

--- src/main/javascript/game/gamescreen.js
+++ src/main/javascript/game/gamescreen.js
@@ -1,37 +1,4 @@
-function ConvertGameTowers(towerObjects) {
-	var that = this;
-	var towers = []
-	towerObjects.forEach(function(towerObject) {
-		var tower = {}
-		var x = towerObject.point.x
-		var y = towerObject.point.y
 
-		tower["point"] = [x,y]
-		tower["radius"] = 50
-		tower["capacity"] = towerObject.capacity
-
-		towers.push(tower);
-	});
-	return towers;
-}
-
-
-function ConvertGameZones(zoneObjects) {
-	var that = this;
-
-	var zones = []
-	zoneObjects.forEach(function(zoneObject) {
-		var zone = {}
-
-		zone["poly"] = zoneObject.poly;
-		zone["population"] = 100;
-		zone["callFactor"] = 1.0;
-		zone["label"] = zoneObject.label;
-		zones.push(zone);
-	});
-
-	return zones;
-}
 
 function createBlock(x,y, width, height) {
     return $P($V(x-width,y-width), $V(x+width,y-height), $V(x+width,y+height), $V(x-width,y+height))
@@ -63,20 +30,8 @@ function ZonePlacer(zone) {
 function GameScreen(src) {
         hack = 0;
   var that = this;
-  console.info("Loading level from: " + src);
-
-  this.currentHour = 2;
 
   this.gameComponents = new Array();
-
-  // Create towers
-  this.towerObjects = [
-  	new Tower($V(70, 130), 10),
-  	new Tower($V(350, 60), 20),
-	new Tower($V(350, 300), 10)
-  ]
-  this.towerObjects.forEach(function (tower) { that.gameComponents.push(tower) } );
-
 
   var bl = new Quad(250,250); var br = new Quad(750,250);
   var tr = new Quad(750,750); var tl = new Quad(250,750);
@@ -97,21 +52,6 @@ function GameScreen(src) {
 
   // Further game logic.
   this.updateComponents = function (screen) {
-    screen.currentHour = (screen.currentHour + 1) % 7;
-
-    for (i in screen.gameComponents) {
-      screen.gameComponents[i].updateComponent(screen.currentHour);
-
-      // Run the simulator
-      zones = ConvertGameZones(screen.zoneObjects);
-      towers = ConvertGameTowers(screen.towerObjects);
-      var s = new Simulator(zones,towers);
-      sim_state = s.simulate();
-
-      zip(sim_state["zones"], screen.zoneObjects, function (x,y) { y.calls = x["calls"] } );
-      zip(sim_state["towers"], screen.towerObjects, function (x,y) { y.utilisation = x["utilisation"] } );
-    }
-
   }
 
   //setInterval(this.updateComponents, 5000, this);
@@ -120,22 +60,9 @@ function GameScreen(src) {
   
   this.draw = function (ctx) {
     ctx.font = "bold 12px sans-serif";
-    for (i=0; i < 7; i++) {
-      if (i == this.currentHour)
-        ctx.fillStyle = "rgba(220, 0, 0, 1)";
-      else
-        ctx.fillStyle = "rgba(220, 220, 220, 1)";
 
-      ctx.fillText(i, i * 80, 500);
-    }
-
-    /*for (var i in this.gameComponents) {
-      var tt1 = Date.now();
-      this.gameComponents[i].draw(ctx);
-      var tt2 = Date.now();
-      console.log("Polygon time: " + (tt2-tt1));
-    }*/
     Timer.start("Polydraw");
+    var i = 0;
     this.quadtree.forEach(Square(0,500,500,1000), function (zone) {
         var tt1 = Date.now();
         zone.draw(ctx);
